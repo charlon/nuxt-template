db2a9566f23cb19c682be3da53634679f2179027
Charlon Palacay <charlon@gmail.com>
2023-01-03 11:42:38 -0800

    Adds Eslint config and modules.

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    parser: "@typescript-eslint/parser",
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:nuxt/recommended",
+    "plugin:vue/vue3-recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "vue/script-setup-no-uses-vars": "off",
+  },
+};
